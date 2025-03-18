9f888f1e69a692dbe78b5f5d5d1ef95989c8d9aa
pravinshedbale1 <pravin.shedbale@outlook.com>
2025-03-18 19:11:52 +0530

    data types and variables

---
+++ 1_Fundamentals/data_types.js
@@ -0,0 +1,16 @@
+let numVar = 10;
+let strVal = "Pravin";
+let boolVar = true;
+let undefinedVar = undefined;
+let nullVar = null;
+let symbolVar = Symbol("symbol");
+let bigIntVar = 1n;
+
+console.log(numVar, typeof numVar);
+console.log(strVal, typeof strVal);
+console.log(boolVar, typeof boolVar);
+console.log(undefinedVar, typeof undefinedVar);
+console.log(nullVar, typeof nullVar);
+console.log(symbolVar, typeof symbolVar);
+
+console.log(bigIntVar, typeof bigIntVar);
---
+++ 1_Fundamentals/value_variable.js
@@ -0,0 +1,4 @@
+let js = "amazing";
+
+console.log(js);
+console.log(7 - 11 + 19 * 2);
